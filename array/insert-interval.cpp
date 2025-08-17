c059cd7eeedfd7bb4e147c871e24835fc8c19d2e
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-08-17 11:33:10 +0530

    LC-insert-interval [Runtime: N/A; Memory: N/A]

---
+++ array/insert-interval.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>>ans;
+       for(int i=0;i<intervals.size();i++){
+           int cs=intervals[i][0];
+           int ce=intervals[i][1];
+           int ns=newInterval[0];
+           int ne=newInterval[1];
+           if(ns>ce || cs>ne){
+               if(ce<ns ){
+                 ans.push_back(intervals[i]);
+               }
+               else{
+                ans.push_back(newInterval);
+                for(int j=i;j<intervals.size();j++){
+                     ans.push_back(intervals[j]);
+                     return ans;
+                }
+
+               }
+           }
+           else{
+              int ma=max(ne,ce);
+              int mi=min(cs,ns);
+              newInterval[0]=mi;
+              newInterval[1]=ma;
+
+           }
+          
+       }
+             ans.push_back(newInterval);
+             return ans;
+    }
+};
