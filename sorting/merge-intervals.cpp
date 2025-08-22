b22077548b31d4acf70d6824b56377bb15811aa1
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-08-22 16:09:45 +0530

    LC-merge-intervals [Runtime: 0 ms; Memory: 23.9 MB]

--- sorting/merge-intervals.cpp
+++ sorting/merge-intervals.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>>ans;
-        vector<int>temp=intervals[0];
-        for(int i=1;i<intervals.size();i++){
-        int cs=intervals[i][0];
-        int ce=intervals[i][1];
+sort(intervals.begin(),intervals.end());  
+    vector<int>temp=intervals[0];
+    vector<vector<int>>sol;
+   
+    for(int i=1;i<intervals.size();i++){
         int ts=temp[0];
         int te=temp[1];
+        int cs=intervals[i][0];
+        int ce=intervals[i][1];
         if(cs>te|| ts>cs){
-            ans.push_back(temp);
-            temp=intervals[i];
+        sol.push_back(temp);
+        temp[0]=intervals[i][0];
+        temp[1]=intervals[i][1];
+
         }
         else{
-          int res=max(ce,te);
-           temp[1]=res;
-            }
+           int res=min(ts,cs);
+           int res2=max(te,ce);
+           temp[0]=res;
+           temp[1]=res2;
+          
         }
-        ans.push_back(temp);
-        return ans;
     }
+    sol.push_back(temp);
+   
+     return sol;
+
+   }
+
 };
