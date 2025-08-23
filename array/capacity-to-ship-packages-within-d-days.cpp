a3a2c117a98035ec2ba07c33c84cf62f9db6bb63
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-08-23 21:34:52 +0530

    LC-capacity-to-ship-packages-within-d-days [Runtime: 11 ms; Memory: 35.2 MB]

---
+++ array/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& arr, int k) {
+         int sum=0;
+        int ans=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+        }
+        int large=*max_element(arr.begin(),arr.end());
+        int low=large;
+        int high=sum;
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            int pages=0;
+             int count=0;
+           for(int i=0;i<arr.size();i++){
+               pages+=arr[i];
+               if(pages>mid){
+                   count++;
+                   pages=arr[i];
+               }
+           }
+           if(count<k){
+               ans=mid;
+               high=mid-1;
+           }
+           else{
+               low=mid+1;
+           }
+            
+        }
+         return ans;
+    
+    }
+};
