c082e9ee428a1bbc7848594e4d19f7102d2d7249
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-08-23 09:28:14 +0530

    LC-majority-element-ii [Runtime: N/A; Memory: N/A]

--- sorting/majority-element-ii.cpp
+++ sorting/majority-element-ii.cpp
@@ -5,13 +5,18 @@ public:
         if(nums.size()==1){
             return nums;
         }
+        
          int ele1=nums[0];
          int ele2=nums[1];
       int freq1=1;
       int freq2=1;
-
+      if(nums.size()==2){
+          temp.push_back(ele1);
+          temp.push_back(ele2);
+          return temp;
+      }
       int count=0;
-      for(int i=2;i<nums.size();i++){
+      for(int i=1;i<nums.size();i++){
         if(nums[i]==ele1){
             freq1++;
         }
@@ -22,6 +27,9 @@ public:
             ele1=nums[i];
             freq1=1;
         }
+      }
+
+      for(int i=2;i<nums.size();i++){
         if(nums[i]==ele2){
             freq2++;
         }
@@ -32,24 +40,36 @@ public:
             ele2=nums[i];
             freq2=1;
         }
-      } 
+      }
+
+        
+
+
          for(int i=0;i<nums.size();i++){
-            if(nums[i]==ele1 || nums[i]==ele2){
+            if(nums[i]==ele1 ){
                 count++;
             }
          }
-        
+         int count2=0;
+         for(int i=0;i<nums.size();i++){
+            if( nums[i]==ele2){
+                count2++;
+            }
+         }
+
+          if(ele1!=ele2){
          if(count>(nums.size()/3)){
-            if(ele1!=ele2){
-          temp.push_back(ele1);
+           temp.push_back(ele1);
+         }
+         if(count2>(nums.size()/3)){
            temp.push_back(ele2);
-          
+         }
          }
          else{
-             temp.push_back(ele2);
+            if(count>(nums.size()/3)){
+               temp.push_back(ele1);
          }
          } 
-         
          return temp;
     }
     
