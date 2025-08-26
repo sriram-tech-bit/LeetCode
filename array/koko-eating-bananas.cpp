f89b2c56107fedb532f70c0680140a9bc6a3b212
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-08-27 00:30:40 +0530

    LC-koko-eating-bananas [Runtime: N/A; Memory: N/A]

---
+++ array/koko-eating-bananas.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& arr, int k) {
+    int n=arr.size();
+
+    sort(arr.begin(),arr.end());
+    int low=1;
+    int high=arr[n-1];
+    int ans=INT_MAX;
+           while(low<=high){
+            int mid=(low+high)/2;
+           
+            if(check(arr,mid,k)){
+                ans=mid;
+               high=mid-1;
+            }
+              else{
+              low=mid+1;
+               }
+            
+            
+           } 
+           return ans;
+    }
+
+  bool check(vector<int>&arr,int mid,int k){
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            int res=(arr[i]+mid-1)/mid;
+            sum+=res;
+        }
+        if(sum<=k){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+  }
+
+
+
+};
