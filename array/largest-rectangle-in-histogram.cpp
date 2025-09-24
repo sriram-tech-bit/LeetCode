3764ebf2c3e9b8b378366e9c0a8ce68f2f5cb57a
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-09-24 22:05:48 +0530

    LC-largest-rectangle-in-histogram [Runtime: 33 ms; Memory: 85.9 MB]

---
+++ array/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+      int n=heights.size();
+      vector<int>p1(n,-1);
+      stack<int>res;
+      for(int i=0;i<n;i++){
+        while(res.size()>0 && heights[res.top()]>=heights[i]){
+            res.pop();
+        }
+        if(res.size()>0){
+            p1[i]=res.top();
+        }
+        
+        res.push(i);
+
+      }
+
+
+      vector<int>p2(n,n);
+      stack<int>res2;
+      for(int i=n-1;i>=0;i--){
+        while(res2.size()>0 && heights[res2.top()]>=heights[i]){
+            res2.pop();
+        }
+        if(res2.size()>0){
+            p2[i]=res2.top();
+        }
+       
+        res2.push(i);
+
+      }
+   int ans=INT_MIN;
+   for(int i=0;i<n;i++){
+      ans=max(ans,heights[i]*(p2[i]-p1[i]-1));
+   }
+
+
+
+    return ans;
+
+    }
+};
