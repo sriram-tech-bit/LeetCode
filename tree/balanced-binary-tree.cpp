acdde69fca2193030f402ab8497ae9f3e345a696
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-11-21 11:34:03 +0530

    LC-balanced-binary-tree [Runtime: 0 ms; Memory: 23.2 MB]

---
+++ tree/balanced-binary-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool isbalance=true;
+    bool isBalanced(TreeNode* root) {
+       height(root);
+          return isbalance;
+    }
+  
+  int height(TreeNode* root){
+    if(root==nullptr){
+        return 0;
+    }
+     int l=height(root->left);
+     int r=height(root->right);
+     if(abs(l-r)>1){
+       isbalance=false;   
+     }
+    return 1+max(l,r);
+
+  }
+
+
+
+};
