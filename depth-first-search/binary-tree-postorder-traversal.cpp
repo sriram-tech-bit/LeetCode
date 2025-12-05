b6429dc5f403e8eff293734ff417e223838c861d
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-12-06 01:07:52 +0530

    LC-binary-tree-postorder-traversal [Runtime: 0 ms; Memory: 10.9 MB]

---
+++ depth-first-search/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+     vector<int>preorder;
+        helper(root,preorder);
+         return preorder;
+        
+
+
+    }
+
+
+    void helper(TreeNode* root ,vector<int>&preorder){
+           if(root==nullptr){
+            return ;
+           }
+            helper(root->left,preorder);
+            helper(root->right,preorder);
+            
+             preorder.push_back(root->val);
+       
+
+    }
+
+    
+};
