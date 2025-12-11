4b12d7e4f82095738c169d410d66b7e8cddbc9f9
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-12-11 23:45:38 +0530

    LC-same-tree [Runtime: 0 ms; Memory: 12.8 MB]

--- breadth-first-search/same-tree.cpp
+++ breadth-first-search/same-tree.cpp
@@ -22,9 +22,9 @@ public:
          if(p->val!=q->val){
             return false;
          }
-         
-      return   isSameTree(p->left,q->left);
-      return   isSameTree(p->right,q->right);
+
+      return   isSameTree(p->left,q->left) &&
+         isSameTree(p->right,q->right);
            
 
 
