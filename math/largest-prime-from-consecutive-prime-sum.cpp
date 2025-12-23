7ed5b1827834c55261418a3a38c871d5eb7b45aa
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-12-23 19:26:08 +0530

    LC-largest-prime-from-consecutive-prime-sum [Runtime: N/A; Memory: N/A]

--- math/largest-prime-from-consecutive-prime-sum.cpp
+++ math/largest-prime-from-consecutive-prime-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int largestPrime(int n) {
-        vector<bool>sol(n,true);
+        vector<bool>sol(n+1,true);
         for(int i=2;i*i<=n;i++){
            if(sol[i]==true){
              for(int j=i*i;j<=n;j+=i){
