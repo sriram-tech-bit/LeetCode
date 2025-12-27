3a65e6b6b055afad23d7321bf2d718171c69abfb
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-12-27 22:13:29 +0530

    LC-maximum-product-of-three-numbers [Runtime: N/A; Memory: N/A]

--- sorting/maximum-product-of-three-numbers.cpp
+++ sorting/maximum-product-of-three-numbers.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-       
-        return nums[n-1]*nums[n-2]*nums[n-3];
+     priority_queue<int>mah;
+       for(auto k:nums){
+          mah.push(k);
+       } 
+    int ans=0;
+      if(mah.size()>=3){
+        int p1=mah.top();
+        mah.pop();
+     int p2=mah.top();
+     mah.pop();
+      int p3=mah.top();
+      mah.pop();
+      ans=(p1*p2*p3);
+      }
 
-    }    
     
+    return ans;
+
+    }
 };
