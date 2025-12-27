93f9eac5d0c37cef22bb7d4f30ffff53713c6875
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2025-12-27 22:37:03 +0530

    LC-maximum-product-of-three-numbers [Runtime: N/A; Memory: N/A]

--- array/maximum-product-of-three-numbers.cpp
+++ array/maximum-product-of-three-numbers.cpp
@@ -9,18 +9,19 @@ public:
     }
     sort(nums.begin(),nums.end(),greater<int>());
     int ans=0;
-    if(count>=2){
-      int p1=nums[0];
-      int p2=nums[nums.size()-1];
-      int p3=nums[nums.size()-2]; 
-      ans=p1*p2*p3;
-    }
-    else{
+     if(count==nums.size() || count==0){
          int p1=nums[0];
          int p2=nums[1] ;
          int p3=nums[2];
          ans=p1*p2*p3;
     }
+    else{
+      int p1=nums[0];
+      int p2=nums[nums.size()-1];
+      int p3=nums[nums.size()-2]; 
+      ans=p1*p2*p3;
+    }
+   
     return ans;
     }
        
