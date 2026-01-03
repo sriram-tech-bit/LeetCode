3a256192e36599108d4dcad7aafbe521d16ad1f4
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-01-03 18:49:49 +0530

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

---
+++ greedy/jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        priority_queue<int>pq;
+        pq.push(nums[0]);
+        int jumps=0;
+         while(pq.size()>0){
+            int k=pq.top();
+            jumps++;
+            pq.pop();
+            if(k==n-1){
+                return jumps;
+            }
+            for(int i=0;i<k;i++){
+                pq.push(i+nums[i]);
+            }
+         }
+        return -1;
+    }
+};
