887b6f55930690aaa92b02b7f4c061bf03a4ac70
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-01-07 19:21:45 +0530

    LC-minimum-subarray-length-with-distinct-sum-at-least-k [Runtime: N/A; Memory: N/A]

--- sliding-window/minimum-subarray-length-with-distinct-sum-at-least-k.cpp
+++ sliding-window/minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -3,6 +3,7 @@ public:
     int minLength(vector<int>& nums, int k) {
         int n=nums.size();
        int ans=INT_MAX;
+       bool flag=false;
         vector<int>sol;
         int sum=0;
       for(int i=0;i<n;i++){
@@ -16,9 +17,13 @@ public:
             ans=min(ans,n);
             
               sum-=sol[0];
+              flag=true;
              sol.erase(sol.begin());
             
           }
+      }
+      if(flag=false){
+         return -1;
       }
       return ans;
     }
