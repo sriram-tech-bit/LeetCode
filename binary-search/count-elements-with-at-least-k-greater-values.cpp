15f18924f9e584a648504195e039f1dc9a1ad9b2
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-01-08 18:49:38 +0530

    LC-count-elements-with-at-least-k-greater-values [Runtime: N/A; Memory: N/A]

--- binary-search/count-elements-with-at-least-k-greater-values.cpp
+++ binary-search/count-elements-with-at-least-k-greater-values.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int countElements(vector<int>& nums, int k) {
+    if(nums.size()==1){
+        return 1;
+      }
      unordered_set<int>hs;
       for(int i=0;i<nums.size();i++){
           hs.insert(nums[i]);
       } 
-      if(nums.size()==1){
-        return 1;
-      }
+      
       vector<int>sol;
-      for(auto k:hs){
-        sol.push_back(k);
+      for(auto m:hs){
+        sol.push_back(m);
       }
       sort(sol.begin(),sol.end());
      int count=0;
      int n=sol.size()-1;
-     for(int i=0;i<n;i++){
+     for(int i=0;i<=n;i++){
           int val=n-i;
           if(val>=k){
             count++;
