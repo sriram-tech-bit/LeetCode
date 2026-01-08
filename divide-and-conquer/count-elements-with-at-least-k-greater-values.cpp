f1910b35281e404ef3a86908dd42dc5cdad094ac
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-01-08 22:34:33 +0530

    LC-count-elements-with-at-least-k-greater-values [Runtime: 132 ms; Memory: 173.8 MB]

--- divide-and-conquer/count-elements-with-at-least-k-greater-values.cpp
+++ divide-and-conquer/count-elements-with-at-least-k-greater-values.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int countElements(vector<int>& nums, int k) {
+    
     int  count=0;
     int n=nums.size();
     sort(nums.begin(),nums.end());
     for(int i=0;i<n;i++){
-       if(nums[n-1]==nums[i]){
-        break;
-       }
-       else{
-       int m=n-(i+1);
-       if(m>=k){
+        
+    int indx=n-(upper_bound(nums.begin(),nums.end(),nums[i])-nums.begin());
+        
+       
+       
+       if(indx>=k){
         count++;
-       }
+       
 
 
     }
