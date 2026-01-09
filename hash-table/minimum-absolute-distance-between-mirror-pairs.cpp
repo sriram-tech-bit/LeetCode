f0e405ef4955a3a8c0d4df54b0ddaec71a013c08
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-01-09 17:40:58 +0530

    LC-minimum-absolute-distance-between-mirror-pairs [Runtime: N/A; Memory: N/A]

--- hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
+++ hash-table/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -2,33 +2,35 @@ class Solution {
 public:
     int minMirrorPairDistance(vector<int>& nums) {
            unordered_map<int,int>hm;
-           vector<int>num(nums.size());
-        for(int i=0;i<nums.size();i++){
-            int k=nums[i];
-                int sum=0;
-                while(k>0){
-                 
-                 sum+=k%10;
-                 k=k/10;
-                }
-              num[i]=sum;
-        } 
            int ans=INT_MAX;
            bool flag=false;
-        for(int i=0;i<num.size();i++){
-           if(hm.find(num[i])!=hm.end() && nums[i]%10!=0){
-                int d=abs(i-hm[i]);
-                ans=min(ans,d);
-                flag=true;
+           for(int i=0;i<nums.size();i++){
+            int k=nums[i];
+           if(hm.find(nums[i])!=hm.end()){
+              int d=abs(i-hm[nums[i]]);
+               ans=min(ans,d);
+               flag=true;
+
            }
            else{
-              hm.insert({num[i],i});
-           }
-        }
-      if(flag==false){
-        return -1;
-      }
+            int num=0;
+               while(k>0){
+             
+               num=num*10+k%10;
 
-        return ans;
+               k=k/10;
+
+               }
+               hm.insert({num,i});
+
+           }
+
+           }
+           if(flag==false){
+            return -1;
+           }
+          
+           return ans;
     }
+       
 };
