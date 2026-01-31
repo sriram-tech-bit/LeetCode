606df9c1c84682ae5daa901afd0a563e03a19e69
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-01-31 12:26:46 +0530

    LC-permutations [Runtime: 2 ms; Memory: 10.6 MB]

--- backtracking/permutations.cpp
+++ backtracking/permutations.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
+  void priper(int i,vector<int>&b,int n,vector<int>&nums,vector<vector<int>>&ans,vector<int>&sol){
+    if(i==n){
+        ans.push_back(sol);
+        return ;
+    }
+    for(int j=0;j<n;j++){
+      if(b[j]==1){
+        b[j]=0;
+        sol[i]=nums[j];
+        priper(i+1,b,n,nums,ans,sol);
+        b[j]=1;
+
+
+      }
+
+    }
+    return ;
+
+  }
+
+   
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-      sort(nums.begin(),nums.end());
-        ans.push_back(nums);
-        if(nums.size()==1){
-            return ans;
-        }
-    
-        while(1){
-        next_permutation(nums.begin(),nums.end());
-        ans.push_back(nums);
-        if(is_sorted(nums.begin(),nums.end(),greater<int>())){
-        return ans;
-
-        }
-
+        int n=nums.size();
+        vector<int>sol(n);
+        vector<int>block(n,1);
+        priper(0,block,n,nums,ans,sol);
+     return ans;
 
         }
 
         
-    }
+    
 };
