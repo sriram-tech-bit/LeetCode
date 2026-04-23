d15ed5980a38e4ca4563c0a2476b0b1ae0b4b95b
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-04-23 10:07:06 +0530

    LC-set-matrix-zeroes [Runtime: 29 ms; Memory: 21.9 MB]

--- hash-table/set-matrix-zeroes.cpp
+++ hash-table/set-matrix-zeroes.cpp
@@ -3,33 +3,27 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
        int n=matrix.size();
        int m=matrix[0].size();
+       vector<pair<int,int>>res;
        for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
            if(matrix[i][j]==0){
-            matrix[i][j]=-1;
-            for(int k=0;k<m;k++){
-                if(matrix[i][k]!=0){
-                  matrix[i][k]=-1;
-               
-                }
-                
-            }
-            for(int l=0;l<n;l++){
-              matrix[l][j]=-1;
+               res.push_back({i,j});
            }
-            
-           }
-           
+        }
 
-        }
        }
-     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(matrix[i][j]==-1){
-                matrix[i][j]=0;
-            }
+
+       for(auto k:res){
+        int i=k.first;
+        int j=k.second;
+        for(int p=0;p<m;p++){
+            matrix[i][p]=0;
         }
-     }
+        for(int l=0;l<n;l++){
+            matrix[l][j]=0;
+        }
+        
+       }
 
        
        
