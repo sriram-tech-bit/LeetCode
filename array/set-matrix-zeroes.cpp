dd3251dae15ea6d2759073d4a92fc500e41e738e
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-04-23 09:56:41 +0530

    LC-set-matrix-zeroes [Runtime: N/A; Memory: N/A]

---
+++ array/set-matrix-zeroes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+       int n=matrix.size();
+       int m=matrix[0].size();
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+           if(matrix[i][j]==0){
+            matrix[i][j]=-1;
+            for(int k=0;k<m;k++){
+                if(matrix[i][k]!=0){
+                  matrix[i][k]=-1;
+               
+                }
+                
+            }
+            for(int l=0;l<n;l++){
+              matrix[l][j]=-1;
+           }
+            
+           }
+           
+
+        }
+       }
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==-1){
+                matrix[i][j]=0;
+            }
+        }
+     }
+
+       
+       
+    }
+};
