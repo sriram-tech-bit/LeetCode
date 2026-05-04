58d61b8258dcca43a25e47ba77ab7f987510ac83
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-05-04 19:08:14 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

---
+++ array/count-primes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool>res(true,n+1);
+        res[0]=res[1]=false;
+
+        for(int i=2;i*i<=n;i++){
+           if(res[i]){
+            for(int j=i*i;j<=n;j+=i){
+                res[j]=false;
+            }
+           }
+        }
+        int count=0;
+
+        for(int i=0;i<=n;i++){
+               if(res[i]){
+                count++;
+               }
+        }
+        return count;
+    
+    }
+};
