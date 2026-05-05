e8c51e4000f62daa3db43052873fd81290972f45
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-05-06 00:06:12 +0530

    LC-distinct-prime-factors-of-product-of-array [Runtime: 7 ms; Memory: 23.4 MB]

--- array/distinct-prime-factors-of-product-of-array.cpp
+++ array/distinct-prime-factors-of-product-of-array.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
-      
-    set<int>s;
-     for(auto num:nums) {
-        for(int i=2;i<=num;i++){
-            while(num>0){
-                if(num%i==0){
-                    s.insert(i);
-                    cout<<i;
-                    num=num/i;
-                }
-                else{
-                    break;
-                }
+       vector<int>spf(1000+1);
+       for(int i=0;i<=1000;i++){
+          spf[i]=i;
+       }
+      for(int i=2;i*i<=1000;i++){
+        if(spf[i]==i){
+        for(int j=i;j*i<=1000;j++){
+            if(spf[j*i]==j*i){
+                spf[j*i]=i;
             }
         }
-     }
 
-       return s.size();
-        
+        }
+      }
+      set<int>s;
+   for(auto num:nums){
+     int k=num;
+      while(k!=1){
+        s.insert(spf[k]);
+        k=k/spf[k];
+      }
+   }
+
+    return s.size();
+
     }
+
+
 };
