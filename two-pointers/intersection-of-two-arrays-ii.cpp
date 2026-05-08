84e687c631a7b7ad99a2be36b8cbeadcb8a23cbe
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-05-08 23:35:46 +0530

    LC-intersection-of-two-arrays-ii [Runtime: N/A; Memory: N/A]

--- two-pointers/intersection-of-two-arrays-ii.cpp
+++ two-pointers/intersection-of-two-arrays-ii.cpp
@@ -3,24 +3,20 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int>res;
         unordered_map<int,int>hm1;
-         unordered_map<int,int>hm2;
+        
          for(auto m:nums1){
              hm1[m]++;
          }
-    for(auto m:nums2){
-             hm2[m]++;
+         for(auto k:nums2){
+            if(hm1[k]>0){
+               res.push_back(k) ;
+               hm1[k]--;
+            }
          }
-     for(auto k:hm1){
-        if(hm2.find(k.first)!=hm2.end()){
-           int m=min(k.second,hm2[k.first]);
-              for(int i=0;i<m;i++){
-                res.push_back(k.first);
-              }   
-            
-            
-        }
-     }
-     return res;
+
+         return res;
+
+    
         
     }
 };
