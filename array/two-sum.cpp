66e4d23ccb86a14f2bdf1d9f89147a0b99c814a9
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-05-12 19:01:33 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.cpp
+++ array/two-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         vector<int>res;
-         int n=nums.size();
-         unordered_map<int,int>hm;
-        
-         
-         
-       for(int i=0;i<n;i++){
-           if(hm.find(target-nums[i])!=hm.end()){
-              res.push_back(hm[target-nums[i]]);
-              res.push_back(i);   
+      unordered_map<int,int>hs;
+      vector<int>res;
+      for(int i=0;i<nums.size();i++){
+         int x=target-nums[i];
+         if(hs.find(x)!=hs.end()){
+              res.push_back(i);
+              res.push_back(hs[x]);;
+         }
+         hs[nums[i]]=i;
 
 
-           }
-           hm[nums[i]]=i;
-           
-       }
-          return res; 
+
+
+      }
+
+      return res;
     }
 };
