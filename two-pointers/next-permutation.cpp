d34dad31d7950b5ae8011ec76a5e9fe412e47bbc
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-07-17 17:02:52 +0530

    LC-next-permutation [Runtime: N/A; Memory: N/A]

--- two-pointers/next-permutation.cpp
+++ two-pointers/next-permutation.cpp
@@ -1,55 +1,31 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& arr) {
-        int n=arr.size();
-        int k=n-1;
-        int flag=0;
-        int s=0;
-        if(n<=1){
-            return ;
-        }
-     for(int i=n-2;i>=0;i--){
-         if(arr[i]<arr[k]){
-            k=i;
-            flag=-1;
-            break;
-         }
-         else{
-           k=i;
-         }
-
-     }
-     if(flag==0){
-        int p1=0;
-        int p2=n-1;
-        while(p1<p2){
-            int temp=arr[p2];
-            arr[p2]=arr[p1];
-            arr[p1]=temp;
-            p1++;
-            p2--;
-        }
-        return ;
-     }
-         for(int j=n-1;j>=0;j--){
-            if(arr[j]>arr[k]){
-                s=j;
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+         int index=-1;
+         for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
                 break;
             }
+         }
+
+         if(index==-1){
+            reverse(nums.begin(),nums.end());
+            
+         }
+         else{
+        
+          for(int i=n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                swap(nums[i],nums[index]);
+                break;
             }
-        swap(arr[k],arr[s]);
-        int p1=k+1;
-        int p2=n-1;
-        while(p1<p2){
-            int temp=arr[p2];
-            arr[p2]=arr[p1];
-            arr[p1]=temp;
-            p1++;
-            p2--;
-        }
+          }
+
 
          
-     
-     return ;
-    } 
+         reverse(nums.begin()+index+1,nums.end());
+         }
+    }
 };
