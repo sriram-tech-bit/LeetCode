ac4d22db0ebc24538cc560e06fcfe7d93c731a9c
sriram-tech-bit <sriramulu.sriram33@gmail.com>
2026-09-08 22:13:33 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

---
+++ string/backspace-string-compare.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>st;
+        stack<int>st2;
+        for(int i=0;i<s.size();i++){
+          if(s[i]=='#'){
+            st.pop();
+          }
+          else{
+            st.push(s[i]);
+          }
+
+        }
+        for(int i=0;i<t.size();i++){
+          if(t[i]=='#'){
+            st2.pop();
+          }
+          else{
+            st2.push(t[i]);
+          }
+
+        }
+        s="";
+      while(!st.empty()){
+        s+=st.top();
+        st.pop();
+
+      }
+      t="";
+while(!st2.empty()){
+        t+=st2.top();
+        st2.pop();
+
+      }
+  cout<<s;
+  cout<<t;
+  return s==t;
+
+
+    }
+};
